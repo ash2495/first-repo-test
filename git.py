96252fd2b85b313f8088e99d34f21acd3ff08f60
Ashish br <ashishbr@gameberrylabs.com>
2026-04-14 15:13:29 +0530

    change1

--- git.py
+++ git.py
@@ -3,3 +3,14 @@ dsbhsdjbfh
 enhsdhk
 int map = new map();
 map.add("key30", "value2");
+vvgcvghchg
+vhgvjghvh
+fsbjsdhbkshfbk
+sbdjshbsj
+snhdskns
+ebhebekj
+qebhibqnj
+qbeqi
+ahbdhjnj
+assert map.get("key30") == "value2";
+sortedMap = new TreeMap<>(map);
